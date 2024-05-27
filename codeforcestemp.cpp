fb3e0258340dc5f4c5dc30c0bcef8b0d5cef3d42
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-05-27 11:58:09 +0530

    template updated

--- codeforcestemp.cpp
+++ codeforcestemp.cpp
@@ -1,25 +1,44 @@
-#include <bits/stdc++.h>
 #include<iostream>
 #include<vector>
 #include<unordered_map>
+#include<map>
+#include<queue>
+#include<deque>
 #include<string.h>
 #include<cmath>
+#include<limits>
+#include<algorithm>
 using namespace std;
 
+/*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
 
-#define INF 1e18
-#define PI 3.141592653589793238462
+#define     INF                 1e18
+#define     PI                  3.141592653589793238462
 
-#define ll long long
-#define llfl(i,s,e) for(long long i=s;i<e;i++)
-#define fl(i,s,e) for(int i=s;i<e;i++)
+#define     ll                  long long
+#define     llfl(i,s,e)         for(long long i=s;i<e;i++)
+#define     fl(i,s,e)           for(int i=s;i<e;i++)
+#define     fa(i,z)             for(auto i:z)
+
+#define     pb                  push_back
+#define     pf                  push_front
+
+#define     vsort(v)            sort(v.begin(),v.end())
+#define     vreverse(v)         reverse(v.begin(),v.end())
+
+#define     mapit(it,d1,d2,map) unordered_map<d1,d2>::iterator it=map.begin()
+#define     vecit(it,dt,vec)    vector<dt>:: iterator it=vec.begin();
 
 
-#define vsort(v) sort(v.begin(),v.end())
-#define vreverse(v) reverse(v.begin(),v.end())
+/*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
 
-#define mapit(it,d1,d2,map) unordered_map<d1,d2>::iterator it=map.begin()
-#define vecit(it,dt,vec) vector<dt>:: iterator it=vec.begin();
+ll gcd(ll a, ll b){ if(a==b)return a;    if(a>b)a-=b;    if(b>a)b-=a;    return gcd(a,b);    }
+void debug(int n) { cout<<"HI - "<<n<<endl; }
+
+/*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
+
+
+
 
 void solve(){
     //code here...
