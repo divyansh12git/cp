5712c64d650f32780ab983387fbec3d896526f26
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-07-21 18:30:30 +0530

    update 21-07-24

--- codeforcestemp.cpp
+++ codeforcestemp.cpp
@@ -44,6 +44,8 @@ using namespace std;
 typedef long long ll;
 typedef long double lld;
 typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<ll> vll;
 typedef pair<int,int> pii;
 typedef pair<long, long> pll;
 
