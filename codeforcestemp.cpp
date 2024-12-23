418416875e53573a62d7d441055c79b087c0ef33
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-12-23 17:24:15 +0530

    update 23/12/24

--- codeforcestemp.cpp
+++ codeforcestemp.cpp
@@ -49,10 +49,12 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef pair<int,int> pii;
 typedef pair<long, long> pll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
 typedef priority_queue<int> pqmax;
 typedef priority_queue<ll> pqmaxll;
- typedef priority_queue<int,vector<int>,greater<int>> pqmin;
- typedef priority_queue<ll,vector<ll>,greater<ll>> pqminll;
+typedef priority_queue<int,vector<int>,greater<int>> pqmin;
+typedef priority_queue<ll,vector<ll>,greater<ll>> pqminll;
 
 
 /*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
@@ -92,12 +94,17 @@ ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) %
 ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
 ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll mod_inverse(ll a,ll M){return expo(a,M-2,M);}
 ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+// void precomp(){fact[0]=1;for(int i=1;i<=N;i++){fact[i]=(fact[i-1]*i)%mod;modinv[i]=power(fact[i],mod-2);}}
+// ll ncr(ll n,ll r){if(r>n)return 0;ll numo=fact[n];ll invdeno=(modinv[r]*modinv[n-r])%mod;ll ans=(numo*invdeno)%mod;return ans;}
+ll ncr2(ll n,ll r){if(n<r)return 0;ll ans=1;fl(i,0,r){ans=(ans*(n-i))/(i+1);}return ans;}
 
 /*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
 
 void debug(int t=1) {cout << "Case #" << t << ": ";}
 void tres(bool t){ t?cout<<"YES":cout<<"NO";cout<<endl; }
+void alice(bool t=1){t?cout<<"Alice":cout<<"Bob";cout<<endl;}
 
 /*_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _*/
 
